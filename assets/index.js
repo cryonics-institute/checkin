05bd0399061a0e476460e19c490317f030eb244c
michaelgill1969 <michaelgill1969@gmail.com>
2019-08-21 18:41:24 -0400

    Add Optimized Eye-Only Logo Images

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,5 @@
 export const LOGO = require('./logo.png')
+export const BLUE_LOGO = require('./logo_blue.png')
+export const GREEN_LOGO = require('./logo_green.png')
+export const ORANGE_LOGO = require('./logo_orange.png')
+export const RED_LOGO = require('./logo_red.png')
