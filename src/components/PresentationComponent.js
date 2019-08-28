2cf5b3e8dbef743d93ef2bc094b7ba27b8498db6
michaelgill1969 <michaelgill1969@gmail.com>
2019-08-27 20:31:44 -0400

    Work on Slide Text and Placement

--- src/components/PresentationComponent.js
+++ src/components/PresentationComponent.js
@@ -25,13 +25,13 @@ const presentation = [
     id: 'slide2',
     data: {
       x: 0,
-      y: 3000,
+      y: 5000,
       rotateZ: 90,
       scale: 3
     },
     content: [
       <div key = 'slide2'>
-        <h4 className = 'centerText'>Try our new app...</h4>
+        <h4 className = 'centerText'>Try our new app</h4>
         <img
           src = { phoneInHand }
           alt = 'Phone in Hand'
@@ -46,8 +46,8 @@ const presentation = [
   {
     id: 'slide3',
     data: {
-      x: 4000,
-      y: 3000,
+      x: 5000,
+      y: 5000,
       rotateZ: 180,
       scale: 5
     },
@@ -56,7 +56,7 @@ const presentation = [
         <p className = 'centerText'>
           With Check-In,<br/>
           you can let a <b>buddy</b> know<br/>
-          if you&#39;re <b>sick</b> or <b>injured</b>...
+          if you&#39;re <b>sick</b> or <b>injured</b>
         </p>
       </div>
     ]
@@ -64,10 +64,10 @@ const presentation = [
   {
     id: 'slide4',
     data: {
-      x: 4000,
-      y: 3000,
-      z: -3000,
-      rotateZ: 300,
+      x: 5000,
+      y: 5000,
+      z: -5000,
+      rotateZ: 270,
       scale: 1
     },
     content: [
@@ -81,17 +81,16 @@ const presentation = [
   {
     id: 'slide5',
     data: {
-      x: 4000,
-      y: 3000,
-      z: -6000,
-      rotateZ: 300,
+      x: 5000,
+      y: 5000,
+      z: -10000,
+      rotateZ: 90,
       scale: 1
     },
     content: [
       <div key = 'slide5'>
         <p className = 'centerText'>
-          and <b className='rotating'>unable</b><br/>
-          to call for <b>help!</b>
+          and <b>unable</b> to call for <b>help!</b>
         </p>
       </div>
     ]
@@ -99,76 +98,157 @@ const presentation = [
   {
     id: 'slide6',
     data: {
-      x: 6700,
-      y: -300,
+      x: 10000,
+      y: 10000,
       scale: 6
     },
     content: [
       <div key = 'slide6'>
-        <p className = 'centerText'><b className='positioning'>Never</b> fear, just <b className='rotating'>check-in</b>,  <b className='scaling'>dear</b>!</p>
+        <img
+          src = { phoneInHand }
+          alt = 'Phone in Hand'
+          className = 'centerImage'
+          height = '100%'
+          width = '100%'
+        />
+        <p className = 'centerText'>Just set an interval ...</p>
       </div>
     ]
   },
   {
     id: 'slide7',
     data: {
-      x: 6300,
-      y: 2000,
+      x: 10000,
+      y: 15000,
       rotateZ: 20,
-      scale: 4
+      scale: 5
     },
     content: [
       <div key = 'slide7'>
-        <p className = 'centerText'>if you miss your check-in time,</p>
+        <img
+          src = { phoneInHand }
+          alt = 'Phone in Hand'
+          className = 'centerImage'
+          height = '100%'
+          width = '100%'
+        />
+        <p className = 'centerText'>
+          ... and your phone will alert you<br/>
+          when it&#39;s time to check in.
+        </p>
       </div>
     ]
   },
   {
     id: 'slide8',
     data: {
-      x: 6000,
-      y: 4000,
-      scale: 2
+      x: 10000,
+      y: 20000,
+      rotateZ: 0,
+      scale: 4
     },
     content: [
       <div key = 'slide8'>
-        <p className = 'centerText'>your Standby will be wired.</p>
+        <p className = 'centerText'>If you miss your check-in,</p>
+        <img
+          src = { phoneInHand }
+          alt = 'Phone in Hand'
+          className = 'centerImage'
+          height = '100%'
+          width = '100%'
+        />
+        <p className = 'centerText'>
+          your buddy will get an alert on her phone<br/>
+          letting her know you might need help!
+        </p>
       </div>
     ]
   },
   {
     id: 'slide9',
     data: {
-      x: 8000,
-      y: 5000,
-      z: -100,
-      rotateX: -40,
-      rotateY: 10,
-      scale: 2
+      x: 10000,
+      y: 25000,
+      rotateZ: 20,
+      scale: 3
     },
     content: [
       <div key = 'slide9'>
-        <p className = 'centerText'><span className='have'>&quot;Pish!</span> <span className='you'>&#39;Tis</span> <span className='noticed'>but a</span> <span className='its'>dream!&quot;</span></p>
-        <p className = 'centerText'>I hear you say?</p>
-        <p className = 'centerText'>&quot;Who&#39;d make up such a thing?&quot;</p>
+        <p className = 'centerText'>Look for us on</p>
+        <p className = 'centerText'>the Apple App Store and Google Play</p>
+        <p className = 'centerText'>coming soon ...</p>
       </div>
     ]
   },
   {
     id: 'slide10',
     data: {
-      x: 3000,
-      y: 6000,
-      z: 4000,
-      rotateX: -70,
-      rotateY: 30,
-      scale: 10
+      x: 10000,
+      y: 30000,
+      scale: 2
     },
     content: [
       <div key = 'slide10'>
-        <p className = 'centerText'>Volunteers!</p>
-        <p className = 'centerText'>... on <b className='rotating'>GitHub</b>,  <b className='scaling'>(yay!)</b>!</p>
-        <p className = 'centerText'>Yea! Open-Source doth ring!</p>
+        <p className = 'centerText'>... but first ...</p>
+      </div>
+    ]
+  },
+  {
+    id: 'slide11',
+    data: {
+      x: 15000,
+      y: 30000,
+      z: 4000,
+      rotateX: 180,
+      scale: 3
+    },
+    content: [
+      <div key = 'slide11'>
+        <h3 className = 'centerText'><b>ATTENTION:</b></h3>
+        <p className = 'centerText'>We need beta-testers!</p>
+        <p className = 'centerText'>We&#39;d like to invite you to</p>
+        <p className = 'centerText'>try our upcoming beta version</p>
+        <p className = 'centerText'>and send us your feedback.</p>
+      </div>
+    ]
+  },
+  {
+    id: 'slide12',
+    data: {
+      x: 15000,
+      y: 30000,
+      z: 2000,
+      rotateX: 90,
+      scale: 3
+    },
+    content: [
+      <div key = 'slide12'>
+        <h3 className = 'centerText'>Please sign-up today!</h3>
+        <p className = 'centerText'>Just grab a card by our poster</p>
+        <p className = 'centerText'>and send us an e-mail</p>
+        <p className = 'centerText'>at the address on the card!</p>
+      </div>
+    ]
+  },
+  {
+    id: 'slide13',
+    data: {
+      x: 15000,
+      y: 30000,
+      z: 0,
+      rotateX: 0,
+      scale: 3
+    },
+    content: [
+      <div key = 'slide13'>
+        <h3 className = 'centerText'>Can you code?</h3>
+        <p className = 'centerText'>We work in <b>React!</b></p>
+        <p className = 'centerText'>Fork a project at:</p>
+        <p className = 'centerText'>
+          <a href = 'https://github.com/cryonics-institute'>
+            https://github.com/cryonics-institute
+          </a>
+        </p>
       </div>
     ]
   }
