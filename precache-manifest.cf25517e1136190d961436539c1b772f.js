7626f4baa25ea99dba1afb591d89e0d1c6b9ef3a
michaelgill1969 <michaelgill1969@gmail.com>
2019-08-29 22:32:57 -0400

    Updates

---
+++ precache-manifest.cf25517e1136190d961436539c1b772f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65680a51a35f1ee3ab69e29a065f082e",
+    "url": "/checkin/index.html"
+  },
+  {
+    "revision": "7694422e7268cfa85f17",
+    "url": "/checkin/static/css/2.f6c8dd44.chunk.css"
+  },
+  {
+    "revision": "fbdb32e2dc7d1968263f",
+    "url": "/checkin/static/css/main.0d663393.chunk.css"
+  },
+  {
+    "revision": "7694422e7268cfa85f17",
+    "url": "/checkin/static/js/2.11b2a6ba.chunk.js"
+  },
+  {
+    "revision": "fbdb32e2dc7d1968263f",
+    "url": "/checkin/static/js/main.5e9a9f91.chunk.js"
+  },
+  {
+    "revision": "40d1fb1e1bfd930bc807",
+    "url": "/checkin/static/js/runtime~main.cec9c333.js"
+  },
+  {
+    "revision": "480fb83d4192540460d37a08c57c76a3",
+    "url": "/checkin/static/media/alfred_feels_poorly.480fb83d.svg"
+  },
+  {
+    "revision": "4dc1f7767f8f5db43506457f78c2a1a6",
+    "url": "/checkin/static/media/patient_alert.4dc1f776.png"
+  },
+  {
+    "revision": "eca7d7870e45f846ca881e1e1af2db58",
+    "url": "/checkin/static/media/patient_home.eca7d787.png"
+  },
+  {
+    "revision": "2a15d0755aec2f5020e7229cd665fb82",
+    "url": "/checkin/static/media/splash.2a15d075.png"
+  },
+  {
+    "revision": "8b4f1652b14f508311158ec8883b00f9",
+    "url": "/checkin/static/media/standby_alert.8b4f1652.png"
+  }
+]);
