b80ec11e04f1f5005eb0dd2091f2dbd726bdd80e
Michael Gill <michaelgill1969@gmail.com>
2019-08-29 20:04:09 -0400

    Revert "Completed Alpha-Version of Presentation"

---
+++ components/PatientHomeComponent.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import { View } from 'react-native'
+import { Button, Slider, Text } from 'react-native-elements'
+import { connect } from 'react-redux'
+import { removeTimers, setTimer, signoutPatient } from '../redux/ActionCreators'
+import { styles } from '../styles/Styles'
+
+const mapStateToProps = state => {
+  return {
+    timer: state.timer
+  }
+}
+
+const mapDispatchToProps = (dispatch) => (
+  {
+    removeTimers: () => dispatch(removeTimers()),
+    setTimer: interval => dispatch(setTimer(interval)),
+    signoutPatient: () => dispatch(signoutPatient())
+  }
+)
+
+class PatientHome extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      interval: this.props.timer.interval
+    }
+
+    this.handleIntervalChange = this.handleIntervalChange.bind(this)
+  }
+
+  handleIntervalChange (value) {
+    this.props.removeTimers()
+    this.props.setTimer(this.state.interval)
+  }
+
+  render () {
+    return (
+      <View style = { styles.container }>
+        <Text h4 style = { styles.title }>Check-In Interval</Text>
+        <Slider
+          maximumValue = { this.props.timer.maximumInterval }
+          minimumValue = { this.props.timer.minimumInterval }
+          onSlidingComplete = { value => this.handleIntervalChange(value) }
+          onValueChange = { value => this.setState({ interval: value }) }
+          step = { 1000 }
+          style = { styles.slider }
+          value = { this.props.timer.interval }
+        />
+        <Text style = { styles.text }>
+          { this.state.interval / 1000 } Seconds
+        </Text>
+        <Button
+          onPress = { () => this.props.signoutPatient() }
+          style = { styles.button }
+          title = "Sign Out"
+        />
+      </View>
+    )
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PatientHome)
---
+++ styles/Styles.js
@@ -0,0 +1,35 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create(
+  {
+    button: {
+      margin: 5
+    },
+    container: {
+      alignItems: 'center',
+      backgroundColor: '#fff',
+      flex: 1,
+      justifyContent: 'center',
+      padding: 20
+    },
+    errorText: {
+      color: 'red',
+      fontSize: 12,
+      margin: 5
+    },
+    paragraph: {
+      margin: 5,
+      textAlign: 'center'
+    },
+    slider: {
+      margin: 5,
+      width: '75%'
+    },
+    text: {
+      margin: 5
+    },
+    title: {
+      margin: 5
+    }
+  }
+)
