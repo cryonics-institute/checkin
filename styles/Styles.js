8acb9c91413311094affd62600fc004eb1b31629
michaelgill1969 <michaelgill1969@gmail.com>
2019-10-03 18:16:57 -0400

    Update Styles for Time Inputs; Add Doc Header

--- styles/Styles.js
+++ styles/Styles.js
@@ -1,10 +1,34 @@
+/**
+ * React Native StyleSheet for the project, Cryonics Check-In.
+ *
+ * @author Michael David Gill <michaelgill1969@gmail.com>
+ * @license
+ * Copyright 2019 Cryonics Institute
+ *
+ * This file is part of Cryonics Check-In.
+ *
+ * Cryonics Check-In is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * Cryonics Check-In is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Cryonics Check-In.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import { Dimensions, StyleSheet } from 'react-native'
 
 export const colors = {
   dark: 'steelblue',
   light: 'aliceblue',
   medium: 'lightsteelblue',
-  error: 'firebrick'
+  error: 'firebrick',
+  transparent: 'transparent'
 }
 
 export const styles = StyleSheet.create(
@@ -46,7 +70,8 @@ export const styles = StyleSheet.create(
     },
     row: {
       alignItems: 'center',
-      flexDirection: 'row'
+      flexDirection: 'row',
+      width: '90%'
     },
     slider: {
       margin: 5,
@@ -72,6 +97,10 @@ export const styles = StyleSheet.create(
     },
     title: {
       margin: 5
+    },
+    transparent: {
+      color: colors.transparent,
+      fontSize: 12
     }
   }
 )
