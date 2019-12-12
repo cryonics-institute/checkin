376f12f0544df93f4d688dff4ff57b2c72b9230e
michaelgill1969 <michaelgill1969@gmail.com>
2019-12-12 04:02:00 -0500

    Send Test Message to Backgrounded iOS App

--- ios/checkin/AppDelegate.m
+++ ios/checkin/AppDelegate.m
@@ -34,23 +34,23 @@
       [[UMModuleRegistryProvider alloc] init]
   ];
   RCTBridge *bridge = [
-    [RCTBridge alloc] initWithDelegate: self launchOptions: launchOptions
+    [RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions
   ];
   RCTRootView *rootView = [
     [RCTRootView alloc] initWithBridge:
-      bridge moduleName: @"checkin" initialProperties: nil
+      bridge moduleName:@"checkin" initialProperties:nil
   ];
   rootView.backgroundColor = [
-    [UIColor alloc] initWithRed: 1.0f green: 1.0f blue: 1.0f alpha: 1
+    [UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1
   ];
 
-  self.window = [[UIWindow alloc] initWithFrame: [UIScreen mainScreen].bounds];
+  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
 
-  [super application: application didFinishLaunchingWithOptions: launchOptions];
+  [super application:application didFinishLaunchingWithOptions:launchOptions];
   
   [FIRApp configure];
   [FIRMessaging messaging].delegate = self;
@@ -59,13 +59,17 @@
     // iOS 10 or later
     // For iOS 10 display notification (sent via APNS)
     [UNUserNotificationCenter currentNotificationCenter].delegate = self;
-    UNAuthorizationOptions authOptions = UNAuthorizationOptionAlert |
-        UNAuthorizationOptionSound | UNAuthorizationOptionBadge;
-    [[UNUserNotificationCenter currentNotificationCenter]
-        requestAuthorizationWithOptions: authOptions
-        completionHandler: ^(BOOL granted, NSError * _Nullable error) {
+    UNAuthorizationOptions authOptions =
+      UNAuthorizationOptionAlert |
+      UNAuthorizationOptionSound |
+      UNAuthorizationOptionBadge;
+    [
+      [UNUserNotificationCenter currentNotificationCenter]
+        requestAuthorizationWithOptions:authOptions
+        completionHandler:^(BOOL granted, NSError * _Nullable error) {
           // ...
-        }];
+        }
+    ];
   } else {
     // iOS 10 notifications aren't available; use iOS 8-9 notifications instead.
     UIUserNotificationType allNotificationTypes = (
@@ -75,9 +79,9 @@
     );
     UIUserNotificationSettings *settings = [
       UIUserNotificationSettings settingsForTypes:
-        allNotificationTypes categories: nil
+        allNotificationTypes categories:nil
     ];
-    [application registerUserNotificationSettings: settings];
+    [application registerUserNotificationSettings:settings];
   }
 
   [application registerForRemoteNotifications];
@@ -85,10 +89,48 @@
   return YES;
 }
 
-- (NSArray<id<RCTBridgeModule>> *)extraModulesForBridge: (RCTBridge *)bridge
+#pragma mark -- Custom Firebase Code
+
+- (void)messaging:(FIRMessaging *)messaging
+  didReceiveRegistrationToken:(NSString *)fcmToken
+{
+  NSLog(@"FCM registration token: %@", fcmToken);
+  // Notify about received token.
+  NSDictionary *dataDict = [
+    NSDictionary dictionaryWithObject:fcmToken forKey:@"token"
+  ];
+  [
+    [NSNotificationCenter defaultCenter] postNotificationName:
+    @"FCMToken" object:nil userInfo:dataDict
+  ];
+  // TODO: If necessary send token to application server.
+  // Note: This callback is fired at each app startup and whenever a new token is generated.
+  
+  [
+    [FIRInstanceID instanceID] instanceIDWithHandler:^(
+      FIRInstanceIDResult * _Nullable result,
+      NSError * _Nullable error
+    )
+    {
+      if (error != nil) {
+        NSLog(@"Error fetching remote instance ID: %@", error);
+      } else {
+        NSLog(@"Remote instance ID token: %@", result.token);
+//        NSString* message = [
+//          NSString stringWithFormat:@"Remote InstanceID token: %@", result.token
+//        ];
+//        self.instanceIDTokenMessage.text = message;
+      }
+    }
+  ];
+}
+
+#pragma mark -- Custom React Native Code
+
+- (NSArray<id<RCTBridgeModule>> *)extraModulesForBridge:(RCTBridge *)bridge
 {
   NSArray<id<RCTBridgeModule>> *extraModules = [
-    _moduleRegistryAdapter extraModulesForBridge: bridge
+    _moduleRegistryAdapter extraModulesForBridge:bridge
   ];
   // You can inject any extra modules that you would like here.
   // More information at:
@@ -96,16 +138,16 @@
   return extraModules;
 }
 
-- (NSURL *)sourceURLForBridge: (RCTBridge *)bridge {
+- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge {
   #ifdef DEBUG
     return [
       [RCTBundleURLProvider sharedSettings]
-        jsBundleURLForBundleRoot: @"index"
-        fallbackResource: nil
+        jsBundleURLForBundleRoot:@"index"
+        fallbackResource:nil
     ];
   #else
     return [
-      [NSBundle mainBundle] URLForResource: @"main" withExtension: @"jsbundle"
+      [NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"
     ];
   #endif
 }
