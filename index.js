f22461e2dd19c1670d3ab4001ada386326ea8c70
michaelgill1969 <michaelgill1969@gmail.com>
2019-12-12 11:22:03 -0500

    Clean Branch

--- index.js
+++
@@ -1,9 +0,0 @@
-import { AppRegistry, Platform } from 'react-native';
-import App from './App';
-
-AppRegistry.registerComponent('checkin', () => App);
-
-if (Platform.OS === 'web') {
-  const rootTag = document.getElementById('root') || document.getElementById('main');
-  AppRegistry.runApplication('checkin', { rootTag });
-}
