1e749d45b4d494ec338dec11d235b7aa2f2e3f08
michaelgill1969 <michaelgill1969@gmail.com>
2019-12-28 17:41:03 -0500

    Add File and Function Headers

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,36 @@
+/**
+ * Firebase Cloud Functions for the project, Cryonics Check-In.
+ *
+ * @author Michael David Gill <michaelgill1969@gmail.com>
+ * @license
+ * Copyright 2019 Cryonics Institute
+ *
+ * This file is part of Cryonics Check-In.
+ *
+ * Cryonics Check-In is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * Cryonics Check-In is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Cryonics Check-In.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 const admin = require('firebase-admin')
 const functions = require('firebase-functions')
 const moment = require('moment')
 
 admin.initializeApp(functions.config().firebase)
 
+/**
+ * Checks user documents in the Firestore for whether and/or which devices
+ * should receive push notifications alerting the user to check in.
+ */
 exports.checkCheckins = functions.pubsub.schedule(
   'every 1 minutes'
 ).onRun(
