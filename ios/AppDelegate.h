912bcdc9db8e092af3a195da01485dae8f25f4d2
michaelgill1969 <michaelgill1969@gmail.com>
2019-12-30 20:34:22 -0500

    Eject from Expo

---
+++ ios/AppDelegate.h
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#import <UIKit/UIKit.h>
+#import <UMReactNativeAdapter/UMModuleRegistryAdapter.h>
+#import <React/RCTBridgeDelegate.h>
+#import <UMCore/UMAppDelegateWrapper.h>
+
+@import Firebase;
+
+@interface AppDelegate:
+  UMAppDelegateWrapper <RCTBridgeDelegate, FIRMessagingDelegate>
+
+@property (nonatomic, strong) UMModuleRegistryAdapter *moduleRegistryAdapter;
+@property (nonatomic, strong) UIWindow *window;
+
+@end
