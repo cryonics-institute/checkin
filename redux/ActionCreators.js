53baec84e3466235ab996b15745b834e298eaf2f
michaelgill1969 <michaelgill1969@gmail.com>
2020-01-01 19:08:32 -0500

    Remove Obsolete Comment

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -1,5 +1,4 @@
 // TODO: Put all your dispatches in an arrow function ... maybe ... so check it out.
-// TODO: It erases the collection if nothing is in it.  Fix that!
 /**
  * Redux action-creators for the project, Cryonics Check-In.
  *
