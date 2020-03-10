1eadf1a797a681d3a6d078d385ba17e09f0a4051
michaelgill1969 <michaelgill1969@gmail.com>
2020-03-10 13:09:57 -0400

    Rebuild Android App w/o Android Studio

--- android/app/src/main/java/com/checkin/MainApplication.java
+++ android/app/src/main/java/com/checkin/MainApplication.java
@@ -3,6 +3,7 @@ package com.checkin;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -40,6 +41,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new AsyncStoragePackage(),
           new ReanimatedPackage(),
           new RNGestureHandlerPackage(),
           new RNScreensPackage(),
