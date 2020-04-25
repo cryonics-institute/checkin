66562fe9c28af9fabb7588e2962374f2df87ca3f
michaelgill1969 <michaelgill1969@gmail.com>
2020-04-25 14:52:27 -0400

    Rename Screen

--- components/TabsComponent.js
+++ components/TabsComponent.js
@@ -64,7 +64,7 @@ class Tabs extends React.Component {
             }
           />
           : <Tab.Screen
-            name = 'Buddy'
+            name = 'BuddySelection'
             component = { BuddySelectionScreen }
             options = {
               {
