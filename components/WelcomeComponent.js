878314e71a0ba7ff7a718e1e53267f1c2ac1a2f4
michaelgill1969 <michaelgill1969@gmail.com>
2020-04-29 20:53:19 -0400

    Move Image Style to Inline
    The React Native doc on the Dimensions API (Facebook Inc., 2020)
    recommends using inline styles with it because its values won't change
    when the view is re-rendered using a style sheet.
    
    Reference:
    Facebook Inc. (2020). _Dimensions_. React Native. Retrieved April 29,
    2020, from https://reactnative.dev/docs/dimensions

--- components/WelcomeComponent.js
+++ components/WelcomeComponent.js
@@ -1,17 +1,32 @@
 import React from 'react'
-import { ActivityIndicator, View } from 'react-native'
+import { ActivityIndicator, Dimensions, View } from 'react-native'
 import { Image } from 'react-native-elements'
 import { BLUE_LOGO } from '../assets'
 import { styles } from '../styles/Styles'
 
 class Welcome extends React.Component {
+  smallestDimension () {
+    const height = Dimensions.get('window').height
+    const width = Dimensions.get('window').width
+
+    if (height < width) {
+      return height
+    } else {
+      return width
+    }
+  }
+
   render () {
     return (
       <View style = { styles.containerSpaced }>
         <Image
-          resizeMode = 'contain'
           source = { BLUE_LOGO }
-          style = { styles.image }
+          style = {
+            {
+              height: this.smallestDimension() * 0.8,
+              width: this.smallestDimension() * 0.8
+            }
+          }
         />
         <ActivityIndicator />
       </View>
