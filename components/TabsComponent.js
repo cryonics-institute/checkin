493368174646b7ca977451328d6a88f35ca69faf
michaelgill1969 <michaelgill1969@gmail.com>
2020-05-25 16:54:51 -0400

    Change Tabs Function Back into Component

--- components/TabsComponent.js
+++ components/TabsComponent.js
@@ -1,84 +1,90 @@
 import React from 'react'
 import { Icon } from 'react-native-elements'
-import { useSelector } from 'react-redux'
+import { connect } from 'react-redux'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { colors, styles } from '../styles/Styles'
 import HomeScreen from './HomeComponent'
 import BuddyScreen from './BuddyComponent'
 import BuddySelectionScreen from './BuddySelectionComponent'
 
-// TODO: buddyIsAdded needs useEffect Hook.
-// TODO: Goes back to home screen after adding listener.
-// TODO: Needs something in Redux that knows which screen it's on.
-function Tabs () {
-  const Tab = createBottomTabNavigator()
-  const buddyIsAdded = useSelector(state => state.buddy.isAdded)
-
-  return (
-    <Tab.Navigator
-      initialRouteName = { buddyIsAdded ? 'Buddy' : 'Home' }
-      backBehavior = 'history'
-      tabBarOptions = {
-        {
-          activeBackgroundColor: colors.dark,
-          activeTintColor: colors.light,
-          keyboardHidesTabBar: true,
-          style: styles.tab
-        }
-      }
-    >
-      <Tab.Screen
-        name = 'Home'
-        component = { HomeScreen }
-        options = {
-          {
-            tabBarLabel: 'Check In',
-            // eslint-disable-next-line react/display-name
-            tabBarIcon: ({ color, size }) => <Icon
-              name = 'check'
-              type = 'material'
-              color = { color }
-              size = { size }
-            />
-          }
-        }
-      />
-      { buddyIsAdded
-        ? <Tab.Screen
-          name = 'Buddy'
-          component = { BuddyScreen }
-          options = {
-            {
-              tabBarLabel: 'Buddies',
-              // eslint-disable-next-line react/display-name
-              tabBarIcon: ({ color, size }) => <Icon
-                name = 'people'
-                type = 'material'
-                color = { color }
-                size = { size }
-              />
-            }
-          }
-        />
-        : <Tab.Screen
-          name = 'BuddySelection'
-          component = { BuddySelectionScreen }
-          options = {
-            {
-              tabBarLabel: 'Buddies',
-              // eslint-disable-next-line react/display-name
-              tabBarIcon: ({ color, size }) => <Icon
-                name = 'people'
-                type = 'material'
-                color = { color }
-                size = { size }
-              />
-            }
-          }
-        />
-      }
-    </Tab.Navigator>
-  )
+const mapStateToProps = state => {
+  return {
+    buddyIsAdded: state.buddy.isAdded
+  }
 }
 
-export default Tabs
+// TODO: Goes back to home screen after adding listener.
+// TODO: Needs something in Redux that knows which screen it's on.
+class Tabs extends React.Component {
+  render () {
+    const Tab = createBottomTabNavigator()
+
+    return (
+      <Tab.Navigator
+        initialRouteName = { this.props.buddyIsAdded ? 'Buddy' : 'Home' }
+        backBehavior = 'history'
+        tabBarOptions = {
+          {
+            activeBackgroundColor: colors.dark,
+            activeTintColor: colors.light,
+            keyboardHidesTabBar: true,
+            style: styles.tab
+          }
+        }
+      >
+        <Tab.Screen
+          name = 'Home'
+          component = { HomeScreen }
+          options = {
+            {
+              tabBarLabel: 'Check In',
+              // eslint-disable-next-line react/display-name
+              tabBarIcon: ({ color, size }) => <Icon
+                name = 'check'
+                type = 'material'
+                color = { color }
+                size = { size }
+              />
+            }
+          }
+        />
+        { this.props.buddyIsAdded
+          ? <Tab.Screen
+            name = 'Buddy'
+            component = { BuddyScreen }
+            options = {
+              {
+                tabBarLabel: 'Buddies',
+                // eslint-disable-next-line react/display-name
+                tabBarIcon: ({ color, size }) => <Icon
+                  name = 'people'
+                  type = 'material'
+                  color = { color }
+                  size = { size }
+                />
+              }
+            }
+          />
+          : <Tab.Screen
+            name = 'BuddySelection'
+            component = { BuddySelectionScreen }
+            options = {
+              {
+                tabBarLabel: 'Buddies',
+                // eslint-disable-next-line react/display-name
+                tabBarIcon: ({ color, size }) => <Icon
+                  name = 'people'
+                  type = 'material'
+                  color = { color }
+                  size = { size }
+                />
+              }
+            }
+          />
+        }
+      </Tab.Navigator>
+    )
+  }
+}
+
+export default connect(mapStateToProps)(Tabs)
