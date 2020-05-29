b95bfd676425e25502fed610f1aed3342e88dc6e
michaelgill1969 <michaelgill1969@gmail.com>
2020-05-29 19:50:52 -0400

    Remove Node Package for Converting Flow to PropTypes

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  plugins: [
-    ['react-flow-props-to-prop-types', { /* options */ }]
-  ],
   presets: [
     '@babel/preset-flow',
     'module:metro-react-native-babel-preset'
