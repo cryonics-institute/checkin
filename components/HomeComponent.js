1c458494594938c5ef0be4c990385d7833263b35
michaelgill1969 <michaelgill1969@gmail.com>
2020-06-05 19:10:32 -0400

    Replace References to "Cryonics Check-In" with "Check-In"

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -8,20 +8,20 @@
  * @license
  * Copyright 2019 Cryonics Institute
  *
- * This file is part of Cryonics Check-In.
+ * This file is part of Check-In.
  *
- * Cryonics Check-In is free software: you can redistribute it and/or modify it
+ * Check-In is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
  * Software Foundation, either version 3 of the License, or (at your option) any
  * later version.
  *
- * Cryonics Check-In is distributed in the hope that it will be useful, but
+ * Check-In is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
  * more details.
  *
  * You should have received a copy of the GNU General Public License along with
- * Cryonics Check-In.  If not, see <https://www.gnu.org/licenses/>.
+ * Check-In.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 import React from 'react'
--- redux/buddyReducer.js
+++ redux/buddyReducer.js
@@ -1,25 +1,25 @@
 /**
- * Redux reducer for the project, Cryonics Check-In, that stores the state for
+ * Redux reducer for the project, Check-In, that stores the state for
  * buddy objects.
  *
  * @author Michael David Gill <michaelgill1969@gmail.com>
  * @license
  * Copyright 2019 Cryonics Institute
  *
- * This file is part of Cryonics Check-In.
+ * This file is part of Check-In.
  *
- * Cryonics Check-In is free software: you can redistribute it and/or modify it
+ * Check-In is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
  * Software Foundation, either version 3 of the License, or (at your option) any
  * later version.
  *
- * Cryonics Check-In is distributed in the hope that it will be useful, but
+ * Check-In is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
  * more details.
  *
  * You should have received a copy of the GNU General Public License along with
- * Cryonics Check-In.  If not, see <https://www.gnu.org/licenses/>.
+ * Check-In.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 import * as ActionTypes from './ActionTypes'
